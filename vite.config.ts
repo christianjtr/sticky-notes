b29f75672527a27381592cedd3b9883968e974de
Christian José Torrealba Rondón <13179028+christianjtr@users.noreply.github.com>
2026-01-27 13:14:13 +0100

    chore: :wrench: Add aliases paths to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,14 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@assets": resolve(__dirname, "./src/assets"),
+      "@app-types": resolve(__dirname, "./src/types"),
+      "@components": resolve(__dirname, "./src/components"),
+      "@hooks": resolve(__dirname, "./src/hooks"),
+      "@styles": resolve(__dirname, "./src/styles"),
+      "@utils": resolve(__dirname, "./src/utils"),
+    },
+  },
 })
